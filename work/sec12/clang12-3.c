57ad60ae2821f27006d8f4264a8fbe54e54cab35
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-07-28 22:32:36 +0900

    finish section 12

---
+++ work/sec12/clang12-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 3
+
+void main() {
+    int* p1 = NULL;
+    double *p2 = NULL;
+    int i;
+
+    p1 = (int*)malloc(sizeof(int)*SIZE);
+    p2 = (double*)malloc(sizeof(double)*SIZE);
+
+    for(i=0; i<SIZE; i++){
+        p1[i] = i;
+        p2[i] = i / 10.0;
+    }
+
+    for(i=0; i<SIZE; i++) {
+        printf("p1[%d]=%d \t p2[%d]=%f\n", i, p1[i], i, p2[i]);
+    }
+
+    free(p1);
+    free(p2);
+}
