e5d5610bdebefa0ecd74659133d45e021aa339e8
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-08-01 23:12:00 +0900

    finish section18

---
+++ work/cpp/sec18/18-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s, t;
+  t = "input value is: ";
+  cout << "please input string: ";
+  cin >> s;
+  cout << t + s << ". " << endl;
+  return 0;
+}
